4977b30fc781e99a65dbf4c3e96b78e8bc24bceb
Richard.W <richardnzworking@gmail.com>
2019-10-08 17:16:18 +1300

    start learning asp.net core web api

---
+++ web_app_demo/Books/Books.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace web_app_demo.Books
+{
+    public partial class Books
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ web_app_demo/Books/testContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace web_app_demo.Books
+{
+    public partial class testContext : DbContext
+    {
+        public testContext()
+        {
+        }
+
+        public testContext(DbContextOptions<testContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Books> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=127.0.0.1;port=3306;user=root;password=a85215263;database=test");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>(entity =>
+            {
+                entity.ToTable("books", "test");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .HasColumnType("int(11)")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
